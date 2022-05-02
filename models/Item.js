702c42ef0ec336817c84a0363a7a5930013b0665
colin brindle <hcolinbrindle@gmail.com>
2022-05-02 16:53:16 -0400

    Defined columns for Item.js model

---
+++ models/Item.js
@@ -0,0 +1,39 @@
+// Importing Model and DataTypes from the sequelize library
+const { Model, DataTypes } = require("sequelize");
+
+// Importing database connection
+const sequelize = require("../config/connection");
+
+// Initialize Game model (table) by extending off Sequelize's Model class
+class Item extends Model {}
+
+Item.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    item_class: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    hp_boost: {
+      type: DataTypes.INTEGER,
+    },
+    str_boost: {
+      type: DataTypes.INTEGER,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "item",
+  }
+);
