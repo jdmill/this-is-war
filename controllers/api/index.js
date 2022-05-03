e33912799d300c8c0353914729c60baa30da3c6b
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-03 12:13:36 -0400

    Routes and auth

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./userRoutes');
-const itemRoutes = require('./itemRoutes');
-const gameRoutes = require('./gameRoutes');
-const championRoutes = require('./championRoutes');
+const userRoutes = require("./userRoutes");
+const itemRoutes = require("./itemRoutes");
+const gameRoutes = require("./gameRoutes");
+const championRoutes = require("./championRoutes");
 
-router.use('/users', userRoutes);
-router.use('/items', itemRoutes);
-router.use('/games', gameRoutes);
-router.use('/champions', championRoutes);
+router.use("/users", userRoutes);
+// router.use('/items', itemRoutes);
+// router.use('/games', gameRoutes);
+// router.use('/champions', championRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const sequelize = require("./config/connection");
 
+const routes = require("./controllers");
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server now live on port: ${PORT}`));
