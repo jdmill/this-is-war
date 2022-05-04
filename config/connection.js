9f79bea62cb117648919da0a933f3603bb908b9e
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-04 09:58:42 -0400

    this is war

--- config/connection.js
+++ config/connection.js
@@ -9,12 +9,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
+    process.env.DB_PASSWORD
   );
 }
 
