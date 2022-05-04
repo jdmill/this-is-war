74ccebb198a32752eafa518ddb48c0f3c879e45c
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-04 10:02:12 -0400

    this is war

--- config/connection.js
+++ config/connection.js
@@ -11,9 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.JAWSDB_URL,
       dialect: "mysql",
-      port: 3306,
     }
   );
 }
