e696d289112a1a8a025a1d9a796c8135124adf39
Seraphina <etanschool@gmail.com>
2022-05-04 21:40:20 -0400

    merge conflict

--- public/js/fight.js
+++ public/js/fight.js
@@ -1,13 +1,22 @@
-const session = require("express-session");
+// const session = require("express-session");
+// const res = require("express/lib/response");
+let champHp = document.querySelector("#champHp");
+let champStr = document.querySelector("#champStr");
+let enemyHp = document.querySelector("#enemyHp");
+let enemyStr = document.querySelector("#enemyStr");
 
-const warGame = async () => {
-    const api = session.user_id;
-    const champion = await fetch(`api/champions/fight/${api}`, {
-        method: "GET",
-        body: JSON.stringify({ hitpoints, strength })
-    });
+const combatWombat = async () => {
+    // const api = session.user_id;
+    // const champion = await fetch(`api/champions/fight/${api}`, {
+    //     method: "GET",
+    //     body: JSON.stringify({ hitpoints, strength })
+    // });
 
-    
+    console.log(wow);
+
+    // enemyHp.innerHTML = opponent.hitpoints;
+    // enemyStr.innerHTML = opponent.strength;
+
 }
 
 const opponent = {
@@ -17,3 +26,6 @@ const opponent = {
 };
 
 
+document
+    .querySelector("#combat")
+    .addEventListener("click", combatWombat);
